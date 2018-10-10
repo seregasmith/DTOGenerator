74cb09ab5f4e62822cc14296e2ec861cdf29aecd
s_smith <s.smith@bk.ru>
2018-10-10 16:48:42 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO get settings from args
+        // TODO parse file to some table structure
+        // TODO generify java-class file with filename and fields, like in the file content
+    }
+}
